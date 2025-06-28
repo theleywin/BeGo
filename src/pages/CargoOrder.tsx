0bc727f2bada7b183285e8e4adf39fe7aebf15d8
Pablo Gómez <123096717+theleywin@users.noreply.github.com>
2025-06-28 16:26:36 -0400

    style: little improvments on medium size of the cards

--- src/pages/CargoOrder.tsx
+++ src/pages/CargoOrder.tsx
@@ -26,7 +26,7 @@ const CargoOrders = () => {
         <main className="flex flex-col">
             <Nav selected={selectedTab} setSelected={setSelectedTab} />
             <SearchBar searchTerm={searchTerm} onSearch={setSearchTerm} />
-            <section className="grid grid-cols-1 md:grid-cols-3 gap-4 items-start justify-center">
+            <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 items-start justify-center">
                 {filteredOrders.map((order) => (
                     <OrderCard key={order._id} order={order} />
                 ))}

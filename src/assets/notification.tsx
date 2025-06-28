e6620f0424ad8f85ad3ff2d16b8058803b28c32f
Pablo Gómez <123096717+theleywin@users.noreply.github.com>
2025-06-28 12:37:54 -0400

    feat: add assets for cargo orders page

---
+++ src/assets/notification.tsx
@@ -0,0 +1,23 @@
+const Notification = () => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="26"
+        fill="none"
+        viewBox="0 0 24 26"
+    >
+        <g clipPath="url(#clip0_10_114)">
+            <path
+                fill="#FE0"
+                d="M16.997 25a1 1 0 0 1-1 1h-8a1 1 0 1 1 0-2h8a1 1 0 0 1 1 1m5-3h-20a2 2 0 0 1-1.725-3.008C.966 17.798 1.997 14.419 1.997 10c0-5.523 4.477-10 10-10s10 4.477 10 10c0 4.416 1.033 7.797 1.727 8.993.364.619.365 1.386.003 2.007a1.97 1.97 0 0 1-1.73 1m0-2c-.966-1.659-2-5.494-2-10a8 8 0 1 0-16 0c0 4.508-1.035 8.343-2 10z"
+            ></path>
+        </g>
+        <defs>
+            <clipPath id="clip0_10_114">
+                <path fill="#fff" d="M0 0h23.998v26H0z"></path>
+            </clipPath>
+        </defs>
+    </svg>
+);
+
+export default Notification;

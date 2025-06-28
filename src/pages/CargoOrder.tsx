29f9ab8b7a5544dd38b459d36a922fac1bf67396
Pablo Gómez <123096717+theleywin@users.noreply.github.com>
2025-06-28 15:55:31 -0400

    feat: make the navigation tabs functional, upcoming -> all orders from orders/upcoming, completed -> only the ones with status_string == Recolección completada, past -> none for now

--- src/pages/CargoOrder.tsx
+++ src/pages/CargoOrder.tsx
@@ -1,30 +1,38 @@
 import Nav from "../components/Nav.tsx";
-import  useFetchUpcoming  from "../hooks/useFetchUpcoming.ts";
+import useFetchUpcoming from "../hooks/useFetchUpcoming.ts";
 import SearchBar from "../components/SearchBar.tsx";
-import OrderCard    from "../components/OrderCard.tsx";
-import  { useState } from "react";
-const CargoOrders = () => {
+import OrderCard from "../components/OrderCard.tsx";
+import { useState } from "react";
 
+const CargoOrders = () => {
     const { ordersUpcoming } = useFetchUpcoming();
     const [searchTerm, setSearchTerm] = useState('');
+    const [selectedTab, setSelectedTab] = useState(0); // 0: Upcoming, 1: Completed, 2: Past
 
-    const filteredOrders = searchTerm
-        ? ordersUpcoming.filter(order =>
+    let filteredOrders = ordersUpcoming;
+    if (selectedTab === 1) {
+        filteredOrders = ordersUpcoming.filter(order => order?.status_string === 'Recolección completada');
+    } else if (selectedTab === 2) {
+        filteredOrders = [];
+    }
+
+    if (searchTerm) {
+        filteredOrders = filteredOrders.filter(order =>
             order.order_number.toString().toLowerCase().includes(searchTerm.toLowerCase())
-        )
-        : ordersUpcoming;
+        );
+    }
+
     return (
         <main className="flex flex-col">
-            <Nav/>
+            <Nav selected={selectedTab} setSelected={setSelectedTab} />
             <SearchBar searchTerm={searchTerm} onSearch={setSearchTerm} />
-            <section className="flex flex-wrap items-center flex-col gap-2 justify-center md:flex-row">
-                {filteredOrders.map((order) => {
-                    return (
-                        <OrderCard key={order._id} order={order} />
-                    )
-                })}
+            <section className="grid grid-cols-1 md:grid-cols-3 gap-4 items-start justify-center">
+                {filteredOrders.map((order) => (
+                    <OrderCard key={order._id} order={order} />
+                ))}
             </section>
         </main>
-    )
-}
-export default CargoOrders
+    );
+};
+
+export default CargoOrders;

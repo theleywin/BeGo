8c4275df1edcc1acf1b87da7463338a3eb92eeab
Pablo Gómez <123096717+theleywin@users.noreply.github.com>
2025-06-28 13:17:59 -0400

    feat: create a hook for fetch upcoming cargos

---
+++ src/components/MenuBar.tsx
@@ -0,0 +1,23 @@
+import { useNavigate, Outlet } from "react-router-dom";
+import Notification from "../assets/notification.tsx";
+import ArrowLeft from "../assets/arrow.tsx";
+
+
+const Header: React.FC = () => {
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <header className="flex items-center justify-between h-[56px] !mx-[46px] !mt-[65px] !mb-[18px]">
+                <button onClick={() => navigate('/')}>
+                    <ArrowLeft/>
+                </button>
+                <h1>{false ? 'Cargo Details' : 'Cargo Orders'}</h1>
+                <Notification/>
+            </header>
+            <Outlet />
+        </>
+    )
+}
+
+export default Header

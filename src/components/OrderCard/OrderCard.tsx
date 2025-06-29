82212ccbf7892b2e8e7a67776d29d0ee7bce5791
Pablo Gómez <123096717+theleywin@users.noreply.github.com>
2025-06-28 21:24:48 -0400

    feat: improve logic on pickup button depending of start time

--- src/components/OrderCard/OrderCard.tsx
+++ src/components/OrderCard/OrderCard.tsx
@@ -1,12 +1,17 @@
+import type { Order } from '../../types/order';
+
 import OrderHeader from "./OrderHeader.tsx";
 import OrderStatusBar from "./OrderStatusBar.tsx";
 import OrderTypeStatus from "./OrderTypeStatus.tsx";
 import OrderDetails from "./OrderDetails.tsx";
 import OrderActions from "./OrderActions.tsx";
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-const OrderCard = ({order}) => {
+
+interface OrderCardProps {
+    order: Order;
+}
+
+const OrderCard = ({order}: OrderCardProps) => {
     const [pickup, dropoff] = Array.isArray(order.destinations) ? order.destinations : [];
 
     const isInTransit = () => order?.status_string === 'Recolección completada';
@@ -16,13 +21,14 @@ const OrderCard = ({order}) => {
     return (
         <div className="!mx-[40px] !mb-[39px] w-[349px] h-[336px] flex flex-col">
             <OrderHeader orderNumber={order.order_number} />
-            <div className="relative bg-[#3534344e] rounded-[20px] h-full">
+            <div className="relative bg-black rounded-[20px]">
                 <OrderStatusBar inTransit={inTransit} />
                 <div className="mb-4 h-full flex flex-col justify-between rounded-[20px] p-[2.5px] relative z-10">
                     <div className="flex flex-col justify-between h-full w-full rounded-[17.5px] bg-[#3534344e]">
                         <OrderTypeStatus inTransit={inTransit} type={order.type} />
                         <OrderDetails pickup={pickup} dropoff={dropoff} />
-                        <OrderActions inTransit={inTransit} orderNumber={order.order_number} />
+                        <OrderActions inTransit={inTransit} orderNumber={order.order_number} start_date={order.start_date} />
+                        {/*<OrderActions inTransit={inTransit} orderNumber={order.order_number} start_date={Date.now() + 36000000} />*/}
                     </div>
                 </div>
             </div>
